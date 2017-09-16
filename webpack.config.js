57ed76e41e19bd5d6627d86c7e453bdb418c5555
Steve Cochrane <steve@stevecochrane.com>
2017-09-16 13:48:08 -0500

    Added some devServer configuration to developmentConfig.

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,42 @@ const commonConfig = {
 	]
 };
 
-module.exports = (env) => {
-	console.log("env", env);
+const productionConfig = () => commonConfig;
 
-	return commonConfig;
+const developmentConfig = () => {
+	const config = {
+		devServer: {
+			//	Enable History API fallback so HTML5 History API-based
+			//	routing works. Good for complex setups.
+			historyApiFallback: true,
+
+			//	Display only errors to reduce the amount of output.
+			stats: "errors-only",
+
+			//	Parse host and port from env to allow customization.
+			//	If you use Docker, Vagrant, or Cloud9, set
+			//	host: options.host || "0.0.0.0"
+			//	0.0.0.0 is available to all network devices,
+			//	unlike default "localhost".
+
+			//	Defaults to "localhost"
+			host: process.env.HOST,
+
+			//	Defaults to 8080
+			port: process.env.PORT
+		}
+	};
+
+	return Object.assign(
+		{},
+		commonConfig,
+		config
+	);
+};
+
+module.exports = (env) => {
+	if (env === "production") {
+		return productionConfig();
+	}
+	return developmentConfig();
 };
