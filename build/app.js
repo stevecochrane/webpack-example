0a240f8fe958985b0542ca61ab18776dc74a1997
Steve Cochrane <steve@stevecochrane.com>
2017-09-21 22:44:55 -0500

    Added a .babelrc file and rebuilt app.js for better backwards compatibility with older browsers.

--- build/app.js
+++ build/app.js
@@ -65,31 +65,37 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__component__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_css__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__main_css__);
 
 
+var _component = __webpack_require__(1);
 
+var _component2 = _interopRequireDefault(_component);
 
-document.body.appendChild(Object(__WEBPACK_IMPORTED_MODULE_0__component__["a" /* default */])());
+__webpack_require__(2);
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+document.body.appendChild((0, _component2.default)());
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = ((text = "Hello world") => {
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+exports.default = (text = "Hello world") => {
 	const element = document.createElement("div");
 	element.innerHTML = text;
 	return element;
-});
-
+};
 
 /***/ }),
 /* 2 */
